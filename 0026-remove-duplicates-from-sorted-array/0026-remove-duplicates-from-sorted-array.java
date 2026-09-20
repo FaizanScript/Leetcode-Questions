87fdb32904e60f8afe8488a55c77cb948047a723
Faizan Ansari <batmanfrommcu@gmail.com>
2026-09-20 19:28:11 +0530

    Time: 1 ms (77.28%), Space: 46.6 MB (81.14%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int uniqueIndex = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[uniqueIndex-1]) {
+                nums[uniqueIndex] = nums[i];
+                uniqueIndex++;
+            }
+        }
+        return uniqueIndex;
+    }
+}
