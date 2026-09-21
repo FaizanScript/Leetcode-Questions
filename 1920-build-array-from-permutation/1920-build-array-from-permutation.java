19c87065ef8fc97a39ca88df841ee34890dc7587
Faizan Ansari <batmanfrommcu@gmail.com>
2026-09-21 17:59:04 +0530

    Time: 2 ms (5.29%), Space: 47.1 MB (52.42%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,10 +1,26 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
 
-        for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[ nums[i] ];
+        // int[] ans = new int[nums.length];
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     ans[i] = nums[nums[i]];
+        // }
+        // return ans;
+
+        // follow up: without using an extra space(i.e., O(1)memory)
+
+        int n = nums.length; // the constraint says that the nums[i] is not bigger than nums length
+
+        // Encoding (storing the original value and the future answer together in the same cell.)
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] + n * (nums[nums[i]] % n);
         }
-        return ans;
+
+        // decode (Remove the original value and keep only the future answer.)
+        for (int i = 0; i < n; i++) {
+            nums[i] /= n;
+        }
+        return nums;
     }
 }
